54c7396f2e7a96027050596063c27d3dd151a9a3
Patrick Thomaz <plthomaz@gmail.com>
2022-07-06 09:52:15 -0300

    Finished project

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,5 +1,5 @@
-import { GetStaticProps } from "next";
-import { getSession, useSession } from "next-auth/react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -57,7 +57,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -85,6 +85,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post,
-    }
+    },
+    redirect: 60 *30, // 30 minutes
   }
 }
